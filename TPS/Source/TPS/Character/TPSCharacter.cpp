04aa1dd6218a0b45d30eb0922ecfdab354614db9
박준서 <pppg12jintpt@gmail.com>
2025-03-25 19:29:45 +0900

    AddAnimation

--- TPS/Source/TPS/Character/TPSCharacter.cpp
+++ TPS/Source/TPS/Character/TPSCharacter.cpp
@@ -2,6 +2,11 @@
 
 
 #include "Character/TPSCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 ATPSCharacter::ATPSCharacter()
@@ -10,10 +15,45 @@ ATPSCharacter::ATPSCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> FindMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/_Art/Characters/Mannequins/Meshes/SKM_Manny.SKM_Manny'"));
-	if (FindMesh.Succeeded()) {
+	if (FindMesh.Succeeded()) 
+	{
 		GetMesh()->SetSkeletalMesh(FindMesh.Object);
 	}
 	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm -> SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->SetRelativeLocation(FVector(0.0f, 70.0f, 90.0f));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+#pragma region InputSystem
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> IMCDefaultRef(TEXT
+	("/Script/EnhancedInput.InputMappingContext'/Game/Input/IMC_Default.IMC_Default'"));
+	if (IMCDefaultRef.Succeeded()) 
+	{
+		IMCDefault = IMCDefaultRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> JumpActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Jump.IA_Jump'"));
+	if (JumpActionRef.Succeeded()) 
+	{
+		JumpAction = JumpActionRef.Object;
+	}
+	static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Move.IA_Move'"));
+	if (MoveActionRef.Succeeded()) 
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+	static ConstructorHelpers::FObjectFinder<UInputAction> TurnActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/Actions/IA_Turn.IA_Turn'"));
+	if (TurnActionRef.Succeeded()) 
+	{
+		TurnAction = TurnActionRef.Object;
+	}
+#pragma endregion
+
 }
 
 // Called when the game starts or when spawned
@@ -21,6 +61,17 @@ void ATPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController) 
+	{
+		auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>
+			(PlayerController->GetLocalPlayer());
+
+		if (Subsystem) 
+		{
+			Subsystem->AddMappingContext(IMCDefault, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -35,5 +86,33 @@ void ATPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	auto* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent) 
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ATPSCharacter::Input_Turn);
+	}
+}
+
+void ATPSCharacter::Input_Move(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+	
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRoation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRoation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRoation).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(ForwardDirection, MovementVector.X);
+	AddMovementInput(RightDirection, MovementVector.Y);
+}
+
+void ATPSCharacter::Input_Turn(const FInputActionValue& InputValue)
+{
+	float XValue = InputValue.Get<float>();
+	AddControllerYawInput(XValue);
 }
 
